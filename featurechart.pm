1190addec643d1059048ef3d480bd19c38e6025b
Dan Blanchard <dan.blanchard@gmail.com>
2008-03-10 04:49:39 +0000

    Bug fixes and added Set::Scalar stuff

--- featurechart.pm
+++ featurechart.pm
@@ -7,6 +7,8 @@ use utf8;
 use Encode;
 use Text::ASCIITable;
 use Set::Scalar;
+use warnings;
+use strict;
 
 # Constructor
 sub new 
@@ -27,6 +29,7 @@ sub read_file
 	my $self = $_[0];
 	my @line;
 	my $first = 1;
+	my $featureKey = "";
 	open FEATURES, $_[1];
 	binmode FEATURES, ":utf8";
 	while (<FEATURES>)
@@ -41,10 +44,15 @@ sub read_file
 				$self->{'phonesToFeatures'}{$line[0]}{$self->{'features'}[$i-1]} = $line[$i];
 				if ($line[$i] ne "0")
 				{
-					if (exists($self->{'featuresToPhones'}{$self->{'features'}[$i]}))
+					$featureKey = $line[$i] . @{$self->{'features'}}[$i-1]; # Stores feature value as +feature or -feature
+					if (exists($self->{'featuresToPhones'}{$featureKey}))
 					{
-						$self->{'featuresToPhones'}{$features[$i]} = push($self->{'featuresToPhones'}{$features[$i]},$line[0])
+						$self->{'featuresToPhones'}{$featureKey}->insert($line[0]);
 					}					
+					else
+					{
+						$self->{'featuresToPhones'}{$featureKey} = Set::Scalar->new([$line[0]]);
+					}
 				}
 			}
 		}
@@ -53,7 +61,7 @@ sub read_file
 			# read features from first line
 			$self->{'outputTable'}->setCols(@line);
 			shift(@line);
-			$self->{'features'} = @line;
+			push(@{$self->{'features'}}, @line);
 			$first = 0;
 		}
 		
@@ -67,9 +75,38 @@ sub output
 	print $self->{'outputTable'};
 }
 
-sub phoneForFeatures
+sub phonesForFeatures
 {
-	$self->
+	my $self = shift;
+	my @featureList = @_;
+	my $featureSet = Set::Scalar->new();
+	my $first = 1;
+	for (my $i = 1; $i < scalar(@featureList); $i++)
+	{
+		if (exists($self->{'featuresToPhones'}{$featureList[$i]}))
+		{
+			if (!$first)
+			{
+				$featureSet = $featureSet->intersect($self->{'featuresToPhones'}{$featureList[$i]});				
+			}
+			else
+			{
+				$featureSet = $featureSet->union($self->{'featuresToPhones'}{$featureList[$i]});				
+				$first = 0;
+			}
+		}
+		else
+		{
+			print "ERROR:\tUnknown feature '$featureList[$i]'\n";
+			exit(0);
+		}
+	}
+	if ($featureSet->size == 0)
+	{
+		print "ERROR: There are no segments in your feature chart that meet the specified criteria.\n";
+		exit(0);
+	}
+	return $featureSet->members();
 }
 
 
