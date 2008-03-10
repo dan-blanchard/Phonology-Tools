ede70dc4dde4bbf00a6e7f3618706c7bc5d0b0df
Dan Blanchard <dan.blanchard@gmail.com>
2008-03-10 03:17:37 +0000

    Lots of changes

--- featurechart.pm
+++ featurechart.pm
@@ -5,6 +5,8 @@ package FeatureChart;
 
 use utf8;
 use Encode;
+use Text::ASCIITable;
+use Set::Scalar;
 
 # Constructor
 sub new 
@@ -12,17 +14,20 @@ sub new
 	my $class = shift;
 	$class = ref $class if ref $class;
 	my $self = {};
-	$self->{'chart'} = ();
+	$self->{'phonesToFeatures'} = ();
+	$self->{'featuresToPhones'} = ();
 	$self->{'features'} = [];
+	$self->{'outputTable'} = Text::ASCIITable->new({headingText => 'Feature Chart' });
 	bless($self, $class);
 	return($self);
 }
 
 sub read_file 
 {
+	my $self = $_[0];
 	my @line;
 	my $first = 1;
-	open FEATURES, shift;
+	open FEATURES, $_[1];
 	binmode FEATURES, ":utf8";
 	while (<FEATURES>)
 	{
@@ -30,20 +35,41 @@ sub read_file
 		@line = split(/\t/);	
 		if (!$first)
 		{
+			$self->{'outputTable'}->addRow(@line);
 			for (my $i = 1; $i < scalar(@line); $i++)
 			{
-				$self->{'chart'}{$line[0]}{$features[$i]} = $line[$i];
+				$self->{'phonesToFeatures'}{$line[0]}{$self->{'features'}[$i-1]} = $line[$i];
+				if ($line[$i] ne "0")
+				{
+					if (exists($self->{'featuresToPhones'}{$self->{'features'}[$i]}))
+					{
+						$self->{'featuresToPhones'}{$features[$i]} = push($self->{'featuresToPhones'}{$features[$i]},$line[0])
+					}					
+				}
 			}
 		}
 		else
 		{
 			# read features from first line
+			$self->{'outputTable'}->setCols(@line);
+			shift(@line);
 			$self->{'features'} = @line;
 			$first = 0;
 		}
 		
 	}
 	close(FEATURES);
+}
+
+sub output
+{
+	my $self = $_[0];
+	print $self->{'outputTable'};
+}
+
+sub phoneForFeatures
+{
+	$self->
 }
 
 
